17a6ca8265563628bd5e6aa4d1235faaf00b97dc
Bikal Rai <bikalrai2785@gmail.com>
2023-01-19 21:47:26 +0545

    reupdated render of comment page

--- src/pages/issue/Comments.jsx
+++ src/pages/issue/Comments.jsx
@@ -1,5 +1,5 @@
 import { LocalDining } from '@mui/icons-material';
-import { Grid, Typography } from '@mui/material';
+import { Avatar, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useReducer, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -8,6 +8,8 @@ import {
     issueDetails,
     issueDetailsReducer,
 } from '../issueInfo/state/commentReducer';
+import './comment.css';
+
 const Comments = () => {
     // const [data, dispatch] = useReducer(issueReducer, initialState);
     const [issueDetail, dispatch] = useReducer(
@@ -52,12 +54,25 @@ const Comments = () => {
     console.log(issueDetail, 'iss');
     return (
         <>
-            <Typography variant="subtitle1" sx={{ backgroundColor: 'red' }}>
+            <Typography
+                variant="subtitle1"
+                sx={{ backgroundColor: 'steelblue' }}
+            >
                 {issueDetail.details.body}
             </Typography>
-            <Typography variant="h6" sx={{ backgroundColor: 'lightblue' }}>
-                {issueDetail.comments.map((comment) => comment.body)}
-            </Typography>
+            {issueDetail.comments.map((comment) => {
+                return (
+                    <Grid className="box">
+                        <Avatar
+                            alt="Remy Sharp"
+                            src={comment.user.avatar_url}
+                        />
+                        <span>{comment.user.login}</span>
+                        <span>{comment.created_at}</span>
+                        <Typography>{comment.body}</Typography>
+                    </Grid>
+                );
+            })}
         </>
     );
 };
